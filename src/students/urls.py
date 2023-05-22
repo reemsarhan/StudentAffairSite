584bfffab4577e81ad0544450be1cb796beb3fe3
Saif El-Din Hazem <ivandorix@gmail.com>
2023-05-22 04:28:00 +0200

    last or not idk i just need to die

--- src/students/urls.py
+++ src/students/urls.py
@@ -1,4 +1,4 @@
-from django.shortcuts import redirect,render
+from django.shortcuts import redirect, render
 from django.urls import path
 from . import views
 
@@ -8,13 +8,16 @@ app_name = 'students'
 urlpatterns = [
     path('', views.studentData),
     path('status/', views.studentList),
+
+
     path('add/', views.studentAddition),
     path('<int:id>/', views.studentInfo, name='studentInfo'),
     path('update/', views.studentUpdate),
     path('assigndep/', views.studentDepart),
     path('students/<int:id>/', views.student_detail_view, name='studentInfo'),
-    path('assign-department/<int:id>/', views.assign_department, name='assign-department'),
-       
- 
+    path('assign-department/<int:id>/',
+         views.assign_department, name='assign-department'),
+
+
 
 ]
--- src/students/views.py
+++ src/students/views.py
@@ -1,47 +1,43 @@
-from django.shortcuts import render, redirect,reverse
+from django.shortcuts import render, redirect, reverse
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponseRedirect
 from .models import Student
+from django.urls import reverse
 
 from django.shortcuts import render, redirect
+
 
 def index(request):
     # Logic for the "Home" link
     return render(request, 'index.html')
 
+
 def student_info(request):
     # Logic for the "search students" link
     return render(request, 'studentInfo.html')
+
 
 def add_student(request):
     # Logic for the "Add New Student" link
     return render(request, 'addStudent.html')
 
+
 def view_status(request):
     # Logic for the "View Status" link
     return render(request, 'allStatus.html')
+
 
 def sign_out(request):
     # Logic for the "Sign Out" link
     # Perform any sign out actions if needed
     return redirect('index')  # Redirect to the home page
 
-# Define other view functions for additional anchor links if needed
 
-
-# def studentList(request):
-#     allStudents = Student.objects.all()  # Get all students from the database
-#     # Store all students in a dictionary
-#     # Store all students in a dictionary
-#     # { 'FrontEnd Name': returned value from the database }
-#     context = {'allStudents': allStudents}
-#     # Pass the students to the template
-#     # 'students/studentList.html',
-#     return render(request, 'studentList.html', context)
 def studentList(request):
     allStudents = Student.objects.all()  # Get all students from the database
     context = {'allStudents': allStudents}
     return render(request, 'studentList.html', context)
+
 
 def studentData(request):
     allStudents = Student.objects.all()  # Get all students from the database
@@ -77,44 +73,40 @@ def student_detail_view(request, id):
         'student': student
     }
     return render(request, 'studentInfo.html', context)
-# def assign_department(request, id):
-#     # Retrieve the student based on the provided ID if needed
-#     student = get_object_or_404(Student, id=id)
-    
-#     # Additional processing or logic
-    
-#     context = {'student': student}
-#     return render(request, 'studentDepart.html', context)
-# def assign_department(request, id):
-#     student = get_object_or_404(Student, id=id)
-#     context = {'student': student}
-#     return render(request, 'studentDepart.html', context)
 
-
-
-# def assign_department(request, id):
-#     student = get_object_or_404(Student, id=id)
-    
-#     if request.method == 'POST':
-#         department = request.POST.get('dep')
-#         student.department = department
-#         student.save()
-#         return HttpResponseRedirect(reverse('students:studentInfo', args=[student.id]))
-    
-#     context = {'student': student}
-#     return render(request, 'studentDepart.html', context)
 
 def assign_department(request, id):
     student = get_object_or_404(Student, id=id)
-    
+
     if request.method == 'POST':
         department = request.POST.get('dep')
         student.department = department
         student.save()
         return HttpResponseRedirect(reverse('students:studentInfo', args=[student.id]))
-    
     context = {'student': student}
     return render(request, 'studentDepart.html', context)
 
- 
-   
+
+def assign_status(request, id):
+    student = get_object_or_404(Student, id=id)
+    if request.method == 'POST':
+        department = request.POST.get('status')
+        student.status = status
+        student.save()
+        return HttpResponseRedirect(reverse('students:studentInfo', args=[student.id]))
+    context = {'student': student}
+    return render(request, 'studentList.html', context)
+
+
+def save_status(request):
+    if request.method == 'POST':
+        for key, value in request.POST.items():
+            if key.startswith('status_'):
+                student_id = key.split('_')[1]
+                student = Student.objects.get(id=student_id)
+                student.status = value
+                student.save()
+        # Replace 'students' with the appropriate URL pattern name for displaying the student list
+        return redirect('students')
+    # Replace 'status.html' with the actual template name
+    return render(request, 'studentList.html')
